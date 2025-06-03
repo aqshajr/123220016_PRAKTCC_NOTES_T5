be2bb833b53fe84f92aa9adadb1848c93ccab546
Aqsha <aqshajauzaarafa@gmail.com>
2025-06-03 22:03:38 +0700

    Fix port

--- backend/index.js
+++ backend/index.js
@@ -14,4 +14,5 @@ app.use(express.json());
 app.use(NoteRoute);
 app.use(UserRoute);
 
-app.listen(5000, ()=> console.log('Server up and running...'))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}...`))
